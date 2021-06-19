1fe8996516aa6ccb3fe0686c347ece56566e1cae
ISHITA-SHAW <83875590+ISHITA-SHAW@users.noreply.github.com>
2021-06-19 20:07:55 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,20 @@
+class Paper{
+    constructor(x,y,radius){
+        var options = {
+            isStatic:false,
+            'restitution':0.3,
+            'friction':0.5,
+            'density':1.2   
+        }
+        this.body = Bodies.rectangle(x,y,radius,radius,options);
+        this.radius = radius;
+        World.add(world,this.body);
+        this.image = loadImage("paper.png");
+    }
+    
+    display(){
+        //ellipseMode(CENTER);
+        fill("purple");
+        image(this.image,this.body.position.x,this.body.position.y,this.radius,this.radius);
+    }
+};
